e205e05eac1e4646f23b1ef2acead98035a54052
Cash Costello <cash.costello@jhuapl.edu>
2017-11-17 13:59:18 -0500

    added a layer for persister to update the source cache

---
+++ src/main/java/edu/jhu/hlt/stretcher/storage/CacheUpdatingPersister.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2012-2017 Johns Hopkins University HLTCOE. All rights reserved.
+ * This software is released under the 2-clause BSD license.
+ * See LICENSE in the project root directory.
+ */
+package edu.jhu.hlt.stretcher.storage;
+
+import edu.jhu.hlt.concrete.Communication;
+import edu.jhu.hlt.stretcher.source.CachingSource;
+import edu.jhu.hlt.stretcher.source.CommunicationSource;
+
+/**
+ * Updates the source cache on calls to store.
+ */
+public class CacheUpdatingPersister implements Persister {
+
+  private final Persister persister;
+  private final CachingSource source;
+
+  public CacheUpdatingPersister(Persister persister, CommunicationSource source) {
+    this.persister = persister;
+    if (source instanceof CachingSource) {
+      this.source = (CachingSource)source;
+    } else {
+      this.source = null;
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see edu.jhu.hlt.stretcher.storage.Persister#store(edu.jhu.hlt.concrete.Communication)
+   */
+  @Override
+  public void store(Communication c) {
+    if (source != null) {
+      source.update(c);
+    }
+    persister.store(c);
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see java.lang.AutoCloseable#close()
+   */
+  @Override
+  public void close() throws Exception {
+    persister.close();
+  }
+
+}
--- src/main/java/edu/jhu/hlt/stretcher/storage/NoOpPersister.java
+++ src/main/java/edu/jhu/hlt/stretcher/storage/NoOpPersister.java
@@ -13,11 +13,19 @@ import edu.jhu.hlt.concrete.Communication;
 public class NoOpPersister implements Persister {
   private static final Logger LOGGER = LoggerFactory.getLogger(NoOpPersister.class);
 
+  /*
+   * (non-Javadoc)
+   * @see edu.jhu.hlt.stretcher.storage.Persister#store(edu.jhu.hlt.concrete.Communication)
+   */
   @Override
   public void store(Communication c) {
     LOGGER.info("Received for storing: " + c.getId());
   }
 
+  /*
+   * (non-Javadoc)
+   * @see java.lang.AutoCloseable#close()
+   */
   @Override
   public void close() throws Exception {}
 
